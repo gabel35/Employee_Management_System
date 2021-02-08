9d866969fb7ec7e91cf1fb995fbe56955527a25a
gabel35 <gabrieljose3135@gmail.com>
2021-02-08 01:33:12 -0500

    created schema and seed.sql

--- schema.sql
+++ schema.sql
@@ -8,13 +8,22 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
-SELECT * FROM topSongs;
-
-
-CREATE TABLE  (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(30) NULL,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10,2) NULL,
+    department_id INT(10) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT, 
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT(10) NULL,
+    manager_id INT(10) NULL,
     PRIMARY KEY (id)
 );
 
 -- SELECT * FROM ;
+
--- seeds.sql
+++ seeds.sql
@@ -1,12 +1,33 @@
 USE institutional_db;
 
 /* Inserting fake data into tables */
-INSERT INTO table (variable)
-VALUES ("");
+INSERT INTO department (name)
+VALUES ("Emergency Medicine"), ("Cardiology"), ("Neurology");
 
-INSERT INTO table (variable)
-VALUES ("");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Attending", 120000.00, 1), 
+("Attending", 138600.86, 2), 
+("Attending", 138600.86, 3), 
+("Surgeon", 156800.00, 2), 
+("Surgeon", 160230.20, 3), 
+("Resident", 96890.10, 1), 
+("Resident", 95000.01, 2), 
+("Resident", 95000.01, 3);
 
-INSERT INTO table (variable)
-VALUES ("");
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Alex", "A", 1), 
+("Brian", "B", 2), 
+("Casey", "C", 3), 
+("Damon", "D", 4), 
+("Erin", "E", 5);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Fabian", "F", 6, 1), 
+("Gabby", "G", 6, 1), 
+("Henry", "H", 6, 1), 
+("India", "I", 7, 2), 
+("Jose", "J", 7, 2), 
+("Kevin", "K", 7, 4), 
+("Liliana", "L", 8, 3), 
+("Maria", "M", 8, 3), 
+("Nidia", "N", 8, 5);
