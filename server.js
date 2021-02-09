f9ecc31d7f4f110429a3582e5debeed2210f2e4c
gabel35 <gabrieljose3135@gmail.com>
2021-02-09 18:58:15 -0500

    view functions

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function viewInfo(){
             `;
             connection.query(query, function(err, res) {
                 if (err) throw err;
-                console.table(res);
+                console.log(cTable.getTable(res));
                 employeeSearch();
             });
         } else if (answer.view === "View Employees by Department"){
@@ -79,15 +79,80 @@ function viewInfo(){
                 `;
                 connection.query(query, { department_name : answer.dept }, function(err, res) {
                     if (err) throw err;
-                    console.table(res);
+                    console.log(cTable.getTable(res));
+                    employeeSearch();
                 });
             }
         } else if (answer.view === "View Employees by Role/Position"){
-            employeeSearch();
+            roles = //tie in to the title variable//
+            function viewEmpRole () {
+                inquirer
+                .prompt ({
+                    name: "role",
+                    type: "list",
+                    message: "Which role/position?",
+                    choices: [`${roles}`]
+                })
+                    var query = `
+                    SELECT employee.id, first_name, last_name, title, salary, department_name 
+                    FROM employee 
+                    INNER JOIN role ON (employee.role_id = role.id) 
+                    INNER JOIN department ON (role.department_id = department.id)
+                    WHERE ?
+                `;
+                connection.query(query, { title : answer.role }, function(err, res) {
+                    if (err) throw err;
+                    console.log(cTable.getTable(res));
+                    employeeSearch();
+                });
+            }
         } else if (answer.view === "View Employees by Manager"){
-            employeeSearch();
-        } else if (answer.view === "View the Total Utilized Budget of a Department"){
-            employeeSearch();
+            managers = //tie in to the manager? variable//
+            function viewEmpMngr () {
+                inquirer
+                .prompt ({
+                    name: "manager",
+                    type: "list",
+                    message: "Which manager?",
+                    choices: [`${managers}`]
+                })
+                    var query = `
+                    SELECT employee.id, first_name, last_name, title, salary, department_name 
+                    FROM employee 
+                    INNER JOIN role ON (employee.role_id = role.id) 
+                    INNER JOIN department ON (role.department_id = department.id)
+                    WHERE ?
+                `;
+                connection.query(query, { manager_name? : answer.manager }, function(err, res) {
+                    if (err) throw err;
+                    console.log(cTable.getTable(res));
+                    employeeSearch();
+                });
+            }
+            
+        // } else if (answer.view === "View the Total Utilized Budget of a Department"){
+        //     departments = //tie in to the department_name variable//
+        //     function viewEmpDept () {
+        //         inquirer
+        //         .prompt ({
+        //             name: "dept",
+        //             type: "list",
+        //             message: "Which department?",
+        //             choices: [`${departments}`]
+        //         })
+        //             var query = `
+        //             SELECT employee.id, first_name, last_name, title, salary, department_name 
+        //             FROM employee 
+        //             INNER JOIN role ON (employee.role_id = role.id) 
+        //             INNER JOIN department ON (role.department_id = department.id)
+        //             WHERE ?
+        //         `;
+        //         connection.query(query, { department_name : answer.dept }, function(err, res) {
+        //             if (err) throw err;
+        //             console.log(cTable.getTable(res));
+        //             employeeSearch();
+        //         });
+        //     }
         } else {
             employeeSearch();
         }
